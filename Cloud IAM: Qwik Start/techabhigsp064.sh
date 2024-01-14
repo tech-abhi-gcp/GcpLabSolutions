ec16ad80e394f029d21d4944403d44f312c50833
TECH-ABHI_GCP <156564342+tech-abhi-gcp@users.noreply.github.com>
2024-01-14 17:38:11 +0530

    Update techabhigsp064.sh

--- Cloud IAM: Qwik Start/techabhigsp064.sh
+++ Cloud IAM: Qwik Start/techabhigsp064.sh
@@ -1,7 +1,7 @@
 gsutil mb -l us -b on gs://$DEVSHELL_PROJECT_ID
 
 
-echo "subscribe to quicklab " > sample.txt
+echo "subscribe to techabhigcp " > sample.txt
 
 
 gsutil cp sample.txt gs://$DEVSHELL_PROJECT_ID
